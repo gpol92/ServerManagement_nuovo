4d53a147e591cbeb06d8e7021ff928045cd1ce66
Gianmarco Polizia <gpolizia4@gmail.com>
2024-01-14 18:01:15 +0100

    Added test for config view

--- controlPanel/tests.py
+++ controlPanel/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from .views import ConfigView
 
 # Create your tests here.
+
+class ConfigViewTest(TestCase):
+    def test_get_context_data(self):
+        request = RequestFactory().get('/')
+        view = ConfigView()
+        view.setup()
+        view.get_context_data()
