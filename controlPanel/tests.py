1df64607e8a6126d88fd5112ca5d45652559015f
Gianmarco Polizia <gianmarco.polizia@keysight.com>
2024-01-15 14:54:50 +0100

    added test

--- controlPanel/tests.py
+++ controlPanel/tests.py
@@ -1,3 +1,9 @@
+from django.db import connections
 from django.test import TestCase
 
-# Create your tests here.
+class MyTestCase(TestCase):
+    def test_access_test_database(self):
+        test_db_connection = connections['default']
+        print(test_db_connection)
+        # Fai qualcosa con il database di test
+        print(connections.databases)
--- serverControl/settings.py
+++ serverControl/settings.py
@@ -87,7 +87,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    },
+    'test': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "test_db.sqlite3",
+    },
 }
 
 
